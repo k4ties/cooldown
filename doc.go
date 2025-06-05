478c7620747c8579d4dfce6ca79439645bd46dee
k4ties <ezzelgavnezzel@gmail.com>
2025-06-05 09:17:05 +0300

    Fix documentation

--- doc.go
+++ doc.go
@@ -2,15 +2,6 @@
 // goal was to make it very customizable and well-documented.
 //
 // Basic is very easy designed and have no features. Unlike others:
-// ValuedHandler and CoolDown, they're adding self to global Processor (Proc var)
-//
-// In theory, user should be allowed to do custom processors without any issues, so, you can try,
-// if you want :)
-// Custom processors is usable in cases, where you don't want to spawn new goroutine, and you
-// already have a ticker, where you can call processor routines.
-//
-// Processor is doing most work, because it checks, if cooldown is expired, and if it is, stops it
-// immediately with StopCauseExpired stop cause.
-// If Close method on Processor will be called, while some Processable is active, stop cause will
-// become StopCauseClosed.
+// ValuedHandler and CoolDown: they're starting timer via time.AfterFunc, that cancels itself, when
+// cooldown expires.
 package cooldown
